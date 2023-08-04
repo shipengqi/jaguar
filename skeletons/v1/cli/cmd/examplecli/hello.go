151bb2dc51d9b53ea917acc12f476f39b0977ada
shipengqi <pooky.shipengqi@gmail.com>
2023-08-04 15:52:59 +0800

    feat: cli skeleton

---
+++ skeletons/v1/cli/cmd/examplecli/hello.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/shipengqi/jcli"
+
+	"github.com/jaguar/cliskeleton/internal/actions/hello"
+	"github.com/jaguar/cliskeleton/internal/actions/hello/options"
+)
+
+const helloCmdDesc = "Example: Say Hello."
+
+func newHelloCmd() *jcli.Command {
+	o := options.New()
+	c := jcli.NewCommand(
+		hello.ActionName,
+		helloCmdDesc,
+		jcli.WithCommandDesc(subdesc(helloCmdDesc)),
+		jcli.WithCommandAliases(hello.ActionNameAlias),
+		jcli.WithCommandCliOptions(o),
+		jcli.WithCommandRunFunc(func(args []string) error {
+			a, err := hello.NewAction(o, args)
+			if err != nil {
+				return err
+			}
+			return a.Execute()
+		}),
+	)
+
+	return c
+}
