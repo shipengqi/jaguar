aea9f814ef2e2f8b1912b00e2329607eb3222e1b
PengQi Shi <pooky.shipengqi@gmail.com>
2024-04-01 10:56:21 +0800

    feat(options): add more options and disable confirm if the values are provided via the falgs (#22)

--- internal/actions/create/ui/summary.go
+++ internal/actions/create/ui/summary.go
@@ -11,8 +11,9 @@ import (
 func ShowSummary(cfg *config.Config) {
 	// Generate content body.
 	contentBody := fmt.Sprintf(
-		"%s\n\nGo web framework/router: %s\n\n%s\n\nIs use golangci-lint to lint your Go code? %s\nIs use GoReleaser to deliver your Go binaries? %s\nIs use GSemver to generate your next semver version? %s",
-		helpers.MakeStyledString(CreateSummaryHeadingBackend, &helpers.StringStyle{Color: ColorGray}),
+		"%s\n\nApplication type: %s\nGo web framework: %s\n\n%s\n\nIs use golangci-lint to lint your Go code? %s\nIs use GoReleaser to deliver your Go binaries? %s\nIs use GSemver to generate your next semver version? %s",
+		helpers.MakeStyledString(CreateSummaryHeadingApp, &helpers.StringStyle{Color: ColorGray}),
+		helpers.MakeStyledString(SupportedProjectTypes[cfg.ProjectType][1], &helpers.StringStyle{Color: ColorBlue}),
 		helpers.MakeStyledString(SupportedGoFrameworks[cfg.GoFramework][1], &helpers.StringStyle{Color: ColorBlue}),
 		helpers.MakeStyledString(CreateSummaryHeadingTools, &helpers.StringStyle{Color: ColorGray}),
 		helpers.MakeStyledString(strconv.FormatBool(cfg.IsUseGolangCILint), &helpers.StringStyle{Color: ColorBlue}),
